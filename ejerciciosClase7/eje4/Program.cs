3e1d2f74cb5281710d23678c6b7f58fd73161ebe
Martinmisiones777 <martinmisiones@gmail.com>
2021-09-04 17:33:15 -0300

    eje4

---
+++ ejerciciosClase7/eje4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace eje4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vocal;
+            Console.WriteLine('ingrese una vocal');
+            vocal = int.Parse(Console.ReadLine());
+
+            switch (vocal)
+            {
+                case 'a':
+                {
+                    Console.WriteLine('A');
+                }break;
+                default:
+                    Console.WriteLine('es una consonante');
+                    break;
+
+            }
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
